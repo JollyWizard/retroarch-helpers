27da2bd37ac454bb50310b510221b08b6db05a72
James Arlow <james.arlow@gmail.com>
2016-08-28 18:26:50 -0400

    unset IFS
    I had trouble with IFS interfering with output in a complex macro scenario
    this suggestion I saw online (source unknown) ended up fixing it.

--- src/bash/helpers/list-core-systems.sh
+++ src/bash/helpers/list-core-systems.sh
@@ -4,7 +4,7 @@
 source build-system-lists.sh
 
 #Parse the list of names into an array and strip them of wrapping quotes. 
-IFS=";"; declare -a SYSTEMS=($RETROARCH_SYSTEMS)
+IFS=";"; declare -a SYSTEMS=($RETROARCH_SYSTEMS); unset IFS
 for i in "${SYSTEMS[@]}"; do
     echo $i | tr -d "\""
 done

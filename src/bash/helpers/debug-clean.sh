3af4dc5ac3e1a7fc8b75434f75f108e896b68123
James Arlow <james.arlow@gmail.com>
2016-08-28 14:47:48 -0400

    removed proof of concept and early testing code

--- src/bash/helpers/debug-clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export RETROARCH_CONFIG=
-
-export LIBRETRO_DIR=
-export LIBRETRO_INFOS=
--- src/bash/helpers/debug-for-each.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo '\echo -e "\
-$core_id \n\t \
-systemname:\t$core_systemname \n\t \
-corename:\t$core_corename \n\t \
-displayname:\t$core_display_name\
-" ; echo ' \
-| source for-each-core.sh

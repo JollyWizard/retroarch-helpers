6d8a5dca82b3eb33552e8adbad4eddacf3c1f882
James Arlow <james.arlow@gmail.com>
2016-08-28 05:35:55 -0400

    Comments to explain complex macro usage
    This use case really pushed my macro to the limit.  
    
    Things got weirder than I thought they would when I started down the macro road, and the odd combination of bash mechanisms, while logical, wasn't very readable.
    
    So I added explanations for posterity.

--- src/helpers/bash/list-core-systems.sh
+++ src/helpers/bash/list-core-systems.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# For each core command:
+# Lists the unique system names by browsing the cores and appending names that have not been found.
 loop='
 echo "$systems" | grep --quiet "${core_systemname}"
 found=$?
@@ -13,8 +15,12 @@ fi
 #echo $found $core_id 
 '
 
+# Pass to macro:
+#   NOTE: Becuase the macro script uses eval(stdin), the modifcations to the variable are lost when the script completes.
+#   The post hook echos the compiled variable and then this script captures the output into a variable of the same name
 systems=$(echo -e "$loop" | source for-each-core.sh 'echo $systems')
 
+#Parse the list of names into an array and strip them of wrapping quotes. 
 IFS=";"; declare -a SYSTEMS=($systems)
 for i in "${SYSTEMS[@]}"; do
     echo $i | tr -d "\""
